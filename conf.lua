ac34f59b6c15d41fe8660733e50ce4e51546b641
AliYagmurGames <104636380+AliYagmurGames@users.noreply.github.com>
2025-04-07 01:24:00 +0300

    Delete conf.lua

--- conf.lua
+++
@@ -1,8 +0,0 @@
-function love.conf(t)
-    t.identity = "love2d-game"
-    t.version = "11.3"
-    t.window.title = "Love2D Game"
-    t.window.width = 1280
-    t.window.height = 720
-    t.console = true
-end
